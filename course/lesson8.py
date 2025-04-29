26b135190773260f72ad40d3d1bd91b2acf30354
filipe2024silva <filipejose6@gmail.com>
2025-04-29 23:25:57 +0100

    lesson10

---
+++ course/lesson8.py
@@ -0,0 +1,13 @@
+name = "Filipe"
+second_name = "Silva"
+age = 38
+born = 1988
+major_age = age >= 18
+height = 1.73
+
+print('Name:', name)
+print('Second Name:', second_name)
+print('Age:', age)
+print('Born:', born)
+print('Major age?', major_age)
+print('Height in meters:', height)
