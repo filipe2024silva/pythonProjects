22e02586ef5638e2eab6fd58341a7da6d16efc38
Filipe Silva <filipe.silva@pintobrasil.com>
2025-04-29 16:16:18 +0100

    more information

--- Example_1/firstProgramme.py
+++ Example_1/firstProgramme.py
@@ -1,5 +1,3 @@
-print("Hello world")
-
 #Types of variables
 name = "Filipe"
 age = 38
@@ -36,3 +34,69 @@ counter = 0
 while counter < 5:
     print("Counting:", counter)
     counter += 1
+    
+#functions
+def welcome(name):
+    print("Welcome", name)
+    
+welcome("Filipe")
+
+#List, dictionaries, tuple
+#Lists
+fruits = ["apple", "banana", "grape"]
+print(fruits[0]) #apple
+print(fruits)
+fruits.append("orange")
+print(fruits)
+
+#Dictionary
+person = {"name": "Ana", "age": 30}
+print(person["name"]) #Ana
+
+#Tuple
+coordenates = (10, 20)
+print(coordenates[1]) #20
+
+#Entry of data
+name = input("Type your name: ")
+print("Hello", name)
+
+def maca():
+    return "Você escolheu maçã."
+
+def banana():
+    return "Você escolheu banana."
+
+def uva():
+    return "Você escolheu uva."
+
+#"switch" using dictionary
+options = {
+    "1": maca,
+    "2": banana,
+    "3": uva
+}
+
+opcao = input("Escolha uma fruta (1- maçã, 2- banana, 3- uva): ")
+acao = options.get(opcao, lambda: "Opção inválida.")
+print(acao())
+
+#Strings
+name = " Filipe Silva"
+print(name.strip().upper())
+
+#Errors
+try:
+    number = int(input("Digite um número: "))
+    print(10 / number)
+except ValueError:
+    print("You digited no number")
+except ZeroDivisionError:
+    print("You can`t to divide for zero")
+    
+#Read and write files
+with open("file.txt", "w") as f:
+    f.write("Olá, Filipe!")
+
+with open("file.txt", "r") as f:
+    print(f.read())
