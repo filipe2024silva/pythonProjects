1d749df53551b031ac17cef200f33d356c6d0152
filipe2024silva <filipejose6@gmail.com>
2025-05-01 20:27:18 +0100

    update lesson 42

---
+++ course/lesson42.py
@@ -0,0 +1,10 @@
+#strings
+phrase = 'The python is programmer language'\
+        'and the python is a good language'
+
+print(phrase)
+
+print(phrase.count('python'))
+
+for word in phrase:
+    print(word)
