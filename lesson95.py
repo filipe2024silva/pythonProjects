6c8d555565fa4234dd7aec0f681d5d75d34dc1ba
filipe2024silva <filipejose6@gmail.com>
2025-05-06 23:41:44 +0100

    lesson 98

--- lesson95.py
+++ lesson95.py
@@ -1,3 +1,19 @@
-#Yield From
 import os
 os.system('cls')
+
+try:
+    a = 18
+    b = 0
+    print('Line 1'[1000])
+    c = a / b
+    print('Line 2')
+except ZeroDivisionError:
+    print('Divide for zero')
+except NameError:
+    print('Name b not defined')
+except (TypeError, IndexError) as error:
+    print('TypeError + IndexError')
+    print('MSG:', error)
+    print('Name:', error.__class__.__name__)
+except Exception:
+    print("Unknown error")
